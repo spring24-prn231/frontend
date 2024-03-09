6a22772188f6832e79b9e2832de81f6524f89eb6
tan-phannhat-se <tan.phannhat.se@gmail.com>
2024-03-10 01:03:34 +0700

    #008 - fix navbar map name

--- birthday-blitz/src/pages/footer-customer/FooterCustomer.jsx
+++ birthday-blitz/src/pages/footer-customer/FooterCustomer.jsx
@@ -11,7 +11,7 @@ const FooterCustomer = () => {
                     <h4 className="text-light mb-4">Địa Chỉ</h4>
                     <p className="mb-2"><i className="fa fa-map-marker-alt me-3"></i>10/76/59 Hoàng Hoa Thám, phường 7, quận Bình Thạnh</p>
                     <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                    <p className="mb-2"><i className="fa fa-envelope me-3"></i>info@example.com</p>
+                    <p className="mb-2"><i className="fa fa-envelope me-3"></i>locttse160275@fpt.edu.vn</p>
                     <div className="d-flex pt-2">
                         <a className="btn btn-square btn-outline-light rounded-circle me-1" href=""><i className="fab fa-twitter"></i></a>
                         <a className="btn btn-square btn-outline-light rounded-circle me-1" href=""><i className="fab fa-facebook-f"></i></a>
@@ -25,7 +25,7 @@ const FooterCustomer = () => {
                     <a className="btn btn-link" href="">Về chúng tôi</a>
                     <a className="btn btn-link" href="">Liên hệ</a>
                     <a className="btn btn-link" href="">Dịch vụ</a>
-                    <a className="btn btn-link" href="">Sản phẩm</a>
+                    <a className="btn btn-link" href="">Thực đơn</a>
                 </div>
                 
             </div>
--- birthday-blitz/src/pages/navbar-customer/NavbarCustomer.jsx
+++ birthday-blitz/src/pages/navbar-customer/NavbarCustomer.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, {useEffect, useRef } from 'react'
 import '../common/customer-css/style.css'
 import '../common/customer-css/bootstrap.min.css'
-import carousel1 from '../../assets/landing-page/panel1.jpg' 
+import carousel1 from '../../assets/landing-page/panel1.jpg'
 import { Link } from 'react-router-dom'
 const NavbarCustomer = () => {
-  return (
-    <div>
+    const initialized = useRef(false)
+    useEffect(() => {
+        if (!initialized.current) {
+            initialized.current = true
+            var listPage = ["aboutus", "service", "cook-table"]
+            var page = window.location.pathname.split("/").pop();
+            if (page == "") {
+                document.getElementById("main").classList.add("active")
+            }
+            listPage.forEach(function(item){
+                if (item == page) {
+                     document.getElementById(page).classList.add("active")
+                }
 
-        <nav className="navbar navbar-expand-lg navbar-dark fixed-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
-            <Link to="index.html" className="navbar-brand ms-4 ms-lg-0">
-                <h1 className="text-primary m-0">TIỆC SINH NHẬT</h1>
-            </Link>
-            <button type="button" className="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-                <div className="navbar-nav mx-auto p-4 p-lg-0">
-                    <Link to="index.html" className="nav-item nav-link active">Trang chủ</Link>
-                    <Link to="/manager/feedback" className="nav-item nav-link">Về chúng tôi</Link>
-                    <Link to="service.html" className="nav-item nav-link">Dịch vụ</Link>
-                    <Link to="product.html" className="nav-item nav-link">Sản phẩm</Link>
-                    {/* <div className="nav-item dropdown">
+            });
+            
+          }
+
+
+    }, []);
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark fixed-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
+                <Link to="index.html" className="navbar-brand ms-4 ms-lg-0">
+                    <h1 className="text-primary m-0">TIỆC SINH NHẬT</h1>
+                </Link>
+                <button type="button" className="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <div className="navbar-nav mx-auto p-4 p-lg-0">
+                        <Link id="main" to="index.html" className="nav-item nav-link">Trang chủ</Link>
+                        <Link id="aboutus" to="/manager/feedback" className="nav-item nav-link">Về chúng tôi</Link>
+                        <Link id="service" to="service.html" className="nav-item nav-link">Dịch vụ</Link>
+                        <Link id="cook-table" to="product.html" className="nav-item nav-link">Thực hiện</Link>
+                        {/* <div className="nav-item dropdown">
                         <Link to="https://mail.google.com/mail/u/0/" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</Link>
                         <div className="dropdown-menu m-0">
                             <Link to="team.html" className="dropdown-item">Đội ngũ</Link>
@@ -28,46 +48,46 @@ const NavbarCustomer = () => {
                             <Link to="404.html" className="dropdown-item">404 Page</Link>
                         </div>
                     </div> */}
-                    <Link to="contact.html" className="nav-item nav-link">Liên hệ</Link>
-                </div>
-                <div className=" d-none d-lg-flex">
-                    <div className="flex-shrink-0 btn-lg-square border border-light rounded-circle">
-                        <i className="fa fa-phone text-primary"></i>
+                        <Link to="contact.html" className="nav-item nav-link">Liên hệ</Link>
                     </div>
-                    <div className="ps-3">
-                        <small className="text-primary mb-0">Liên hệ với chúng tôi</small>
-                        <p className="text-light fs-5 mb-0">+012 345 6789</p>
+                    <div className=" d-none d-lg-flex">
+                        <div className="flex-shrink-0 btn-lg-square border border-light rounded-circle">
+                            <i className="fa fa-phone text-primary"></i>
+                        </div>
+                        <div className="ps-3">
+                            <small className="text-primary mb-0">Liên hệ với chúng tôi</small>
+                            <p className="text-light fs-5 mb-0">+012 345 6789</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
 
-        <div className="container-fluid p-0 wow fadeIn" data-wow-delay="0.1s">
-        <div className="owl-carousel header-carousel position-relative">
-            <div className="owl-carousel-item position-relative">
-                <img className="img-fluid" src={carousel1} alt="" />
-                <div className="owl-carousel-inner">
-                    <div className="container">
-                        <div className="row justify-content-start">
-                            <div className="col-lg-8">
-                                <p className="text-primary text-uppercase fw-bold mb-2">// Team LOC12345</p>
-                                <h1 className="display-1 text-light mb-4 animated slideInDown">TỔ CHỨC SINH NHẬT</h1>
-                                <p className="text-light fs-5 mb-4 pb-3">Chúng tôi tự hào là đối tác đáng tin cậy trong việc tổ chức tiệc sinh nhật, tạo nên những khoảnh khắc đặc biệt và ngọt ngào, để bữa tiệc trở thành kỷ niệm không thể quên.</p>
-                                <a href="" className="btn btn-primary rounded-pill py-3 px-5">TÌM HIỂU THÊM</a>
+            <div className="container-fluid p-0 wow fadeIn" data-wow-delay="0.1s">
+                <div className="owl-carousel header-carousel position-relative">
+                    <div className="owl-carousel-item position-relative">
+                        <img className="img-fluid" src={carousel1} alt="" />
+                        <div className="owl-carousel-inner">
+                            <div className="container">
+                                <div className="row justify-content-start">
+                                    <div className="col-lg-8">
+                                        <p className="text-primary text-uppercase fw-bold mb-2">// Team LOC12345</p>
+                                        <h1 className="display-1 text-light mb-4 animated slideInDown">TỔ CHỨC SINH NHẬT</h1>
+                                        <p className="text-light fs-5 mb-4 pb-3">Chúng tôi tự hào là đối tác đáng tin cậy trong việc tổ chức tiệc sinh nhật, tạo nên những khoảnh khắc đặc biệt và ngọt ngào, để bữa tiệc trở thành kỷ niệm không thể quên.</p>
+                                        <a href="" className="btn btn-primary rounded-pill py-3 px-5">TÌM HIỂU THÊM</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+
+
         </div>
-    </div>
-        
-       
-    
-    </div>
-    
-    
-  )
+
+
+    )
 }
 
 export default NavbarCustomer;
